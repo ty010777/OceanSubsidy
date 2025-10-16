35dc54a6c035f29571960d2c0007b07a0bfce99e
jerrychen <jerrychen@gis.tw>
2025-10-16 18:27:22 +0800

    路徑設定 請參考使用

--- EmailApi/Program.cs
+++ EmailApi/Program.cs
@@ -12,6 +12,13 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+// 從設定檔讀取 PathBase (若有設定才套用)
+var pathBase = builder.Configuration["PathBase"];
+if (!string.IsNullOrEmpty(pathBase))
+{
+    app.UsePathBase(pathBase);
+}
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
