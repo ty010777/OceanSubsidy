fe19c419741e1a5ba9d6f43e245f8da79d46b4bf
jrealm <jrealm@gmail.com>
2025-11-28 09:23:35 +0800

    UI 調整

--- components/src/components/ProjectTitle.vue
+++ components/src/components/ProjectTitle.vue
@@ -3,9 +3,9 @@
         <div class="top-block">
             <div class="d-flex align-items-center gap-3">
                 <span>［{{ store[props.type].type }}］{{ store[props.type].id }}/{{ store[props.type].name }}</span>
-<!--
-                <span class="tag tag-pale-green">待辦事項：請填寫契約資料</span>
--->
+                <span class="tag tag-light-gray" v-if="store[type].status === 91">計畫已結案</span>
+                <span class="tag tag-light-pink" v-else-if="store[type].status === 99">計畫已終止</span>
+                <span class="tag tag-pale-green" v-else-if="todo">待辦事項：{{ todo }}</span>
             </div>
             <a class="btn btn-teal-dark" :href="`../AuditRecords.aspx?ProjectID=${id}`" target="_blank">
                 查核紀錄
@@ -21,4 +21,9 @@
     });
 
     const store = useProgressStore();
+    const todo = ref();
+
+    onMounted(() => {
+        api.system("getProjectTodoTask", { ID: props.id }).subscribe((res) => todo.value = res?.TaskName);
+    });
 </script>
